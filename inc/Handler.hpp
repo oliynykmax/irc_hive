3900c30db9818ab585ae0e8beaf5607155d448f7
jtuomi <jtuomi@student.hive.fi>
2025-08-25 16:26:03 +0300

    I ended up cleaning main a bit while at it. Factored out the extra functions to their own Handler class.

---
+++ inc/Handler.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include "RecvParser.hpp"
+#include "CommandDispatcher.hpp"
+#include "Client.hpp"
+#include "Server.hpp"
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+
+extern Server *irc;
+
+class Handler {
+	private:
+		Handler() = delete;
+	public:
+		static void clientWrite(int fd);
+		static void clientDisconnect(int fd);
+		static void acceptClient(int fd);
+
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include "Server.hpp"
-#include "RecvParser.hpp"
-#include "CommandDispatcher.hpp"
+#include "Handler.hpp"
 #include <iostream>
 #include <stdexcept>
 #include <sys/socket.h>
@@ -14,49 +13,6 @@ Server *irc;
 
 namespace {
 	volatile std::sig_atomic_t gSigStatus = 0;
-}
-
-void clientWrite(int fd) {
-	int messageLen = 0;
-	string msg;
-	vector<char> buf(BUFSIZ);
-	queue<unique_ptr<Message>> msg_queue;
-	RecvParser	parser(msg_queue);
-	CommandDispatcher	dispatcher;
-
-	messageLen = recv(fd, &buf[0], buf.size(), 0);
-	if (messageLen == -1)
-		throw runtime_error("Failure receiving message");
-	parser.feed(&buf[0], messageLen);
-	while (!msg_queue.empty())
-	{
-		const unique_ptr<Message> &msg = msg_queue.front();
-		dispatcher.dispatch(msg);
-		msg_queue.pop();
-	}
-	send(fd, "Message received\n", 18, 0);
-}
-
-void clientDisconnect(int fd) {
-	cout << "Client with socket" << fd << " disconnected" << endl;
-}
-
-void acceptClient(int socket) {
-	int fd;
-	struct sockaddr_in remote{};
-	socklen_t remoteLen{};
-
-	fd = accept(socket, (struct sockaddr*) &remote, &remoteLen);
-
-	if (fd > 0) {
-		cout << "A client connected with fd nbr " << fd << " connected" << endl;
-		Client client(fd);
-		irc->addClient(client);
-		irc->registerHandler(fd, EPOLLIN, clientWrite);
-		irc->registerHandler(fd, EPOLLRDHUP | EPOLLHUP, clientDisconnect);
-	} else {
-		throw runtime_error("Failed creating a new TCP connection to client");
-	}
 }
 
 int main(int argc, char *argv[]) {
@@ -112,7 +68,8 @@ int main(int argc, char *argv[]) {
 	signal(SIGQUIT, [](int) { gSigStatus = 1; });
 
     irc->addOwnSocket(sock);
-    irc->registerHandler(sock, EPOLLIN, [](int socket) { acceptClient(socket); });
+    irc->registerHandler(sock, EPOLLIN, [](int socket) { Handler::acceptClient(socket); });
+
 	while (!gSigStatus) {
 		try {
 			irc->poll();
